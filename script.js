b0beee381a568215b4804d182c31f7ec59e7ca6e
SWATI <swatisagarikasital@gmail.com>
2025-06-10 22:27:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
